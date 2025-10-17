e43da5e9c7eefcf6065817c6c0f49b4b5940059e
Juan Caro <carovaquerojuan@gmail.com>
2025-10-17 16:30:56 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import HeaderComponent from "./HeaderComponent"; // adjust path as needed
+
+function App() {
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <h2>Hello everyone</h2>
+    </>
+  );
+}
+
+export default App;
